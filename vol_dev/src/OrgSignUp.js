6d4221a1f0aff1e8000010f129e64c7f31355782
Gokul Sivadasan <gokulsivadasan@hotmail.com>
2021-03-06 12:46:10 -0800

    updated files and prepared organization sign up form

---
+++ vol_dev/src/OrgSignUp.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+class OrgSignUp extends React.Component{
+    constructor() {
+        super()
+    }
+    render() {
+        return (
+            <div className="orgsignup">
+                <div className="form-field">
+                    <label htmlFor="orgname">Organization Name</label>
+                    <input type="text" id = "orgname"/><br />
+                </div>
+                <div className="form-field">
+                    <label htmlFor="orgemail">Organization Email</label>
+                    <input type="text" id = "orgemail"/><br />
+                </div>
+                <div className="form-field">
+                <label htmlFor="orgpass">Enter Password</label>
+                <input type="password" id = "orgpass"/><br />
+                </div>
+                <div className="form-field">
+                <label htmlFor="orgcpass">Enter Password</label>
+                <input type="password" id = "orgcpass"/><br />  
+                </div>
+                <div className="form-field">
+                <label htmlFor="orgweb">Organization Website</label>
+                <input type="text" id = "orgweb"/><br />
+                </div>
+            </div>
+        )
+    }
+}
+export default OrgSignUp;
