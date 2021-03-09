cf3ed6a59a309b603be7a8a50b03a1be508a8306
Gokul Sivadasan <gokulsivadasan@hotmail.com>
2021-03-09 13:31:47 -0800

    Sponsors/ Connect/ Header

---
+++ vol_dev/src/Home/Sponsors.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Sponsors = () => {
+    return (
+        <div>
+            <h1>Our Sponsors</h1>
+            <p>
+                Our sponsors make it so many amazing matches happen. 
+                By sponsoring, you encourage our developers by providing incentives to write code for good. 
+                You may sponsor us finiancially or by providing products and/or services to our developers.
+            </p>
+            <p>
+                If you would like to support us, you can fill out our contact form and we can chat!
+            </p>
+        </div>
+    )
+}
+
+export default Sponsors;
