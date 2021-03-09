093cf7df9eecce392e5cc067df0a0260d079b348
Gokul Sivadasan <gokulsivadasan@hotmail.com>
2021-03-09 12:58:01 -0800

    Header for home

--- vol_dev/src/Home/Header.js
+++ vol_dev/src/Home/Header.js
@@ -3,7 +3,7 @@ import {Link} from 'react-scroll'
  
 const Header = () => {
         return (
-        <header className="header">
+        <div className="home-info">
             <nav className='nav'>
                 <div className='navbar'>
                     <a href="/">home</a>
@@ -14,7 +14,19 @@ const Header = () => {
                     <a href="/">sign up</a>
                 </div>
             </nav>
-        </header>
+        <div className="info">
+            <h1>WELCOME TO SOMETHING GREAT</h1>
+            <h2>A chance for non-profits to expand digitally, a chance for developers to do good.</h2>
+            <h4>At Something Something, we give developers the opportunity to write code for good. 
+                Our platform allows non-profits to create projects and find the perfect developers for them. 
+                Not only will every line of code written go towards a worthy cause, 
+                but we have a gamified system that awards our developers with amazing products and opportunities. </h4>
+        </div>
+        <a href="/"> Join Our Community</a>
+        <div className="projects-home">
+            <h1>Awesome Projects Built on Smth Smth </h1>
+        </div>
+        </div>
     )
 }
  
