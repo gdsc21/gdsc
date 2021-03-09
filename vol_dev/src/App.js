aa5e3a828e5c7aef16bdadd482dfb186afe5c421
Nadeen <nadeen@ualberta.ca>
2021-03-09 14:31:26 -0700

    deleted a bunch of crap

--- vol_dev/src/App.js
+++ vol_dev/src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import Home from "./Home/Home";
+import React from "react";
+import Home from "./home/home";
 
-const App =() => {
-    return(
-        <div>
-            <Home />
-        </div>
-    )
-}
+const App = () => {
+	return (
+		<div>
+			<Home />
+		</div>
+	);
+};
 
 export default App;
