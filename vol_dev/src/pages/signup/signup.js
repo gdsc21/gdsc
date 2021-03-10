445091033cff85f2448e40f1a5042a10ff76f0d4
Nadeen <nadeen@ualberta.ca>
2021-03-10 15:27:33 -0700

    something

--- vol_dev/src/pages/signup/signup.js
+++ vol_dev/src/pages/signup/signup.js
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-import "./scss/signup.scss";
-
-const OrgSignUp = () => {
-	return (
-		<div className="orgsignup">
-			<div className="form-field">
-				<label htmlFor="orgname">Organization Name</label>
-				<input type="text" id="orgname" />
-			</div>
-			<div className="form-field">
-				<label htmlFor="orgemail">Organization Email</label>
-				<input type="text" id="orgemail" />
-			</div>
-			<div className="form-field">
-				<label htmlFor="orgpass">Enter Password</label>
-				<input type="password" id="orgpass" />
-			</div>
-			<div className="form-field">
-				<label htmlFor="orgcpass">Enter Password</label>
-				<input type="password" id="orgcpass" />
-			</div>
-			<div className="form-field">
-				<label htmlFor="orgweb">Organization Website</label>
-				<input type="text" id="orgweb" />
-			</div>
-			<button className="signupButton">Sign Up</button>
-=======
 import React from "react";
 import "./scss/signup.scss";
 import logo from "../../img/logo.svg";
@@ -36,13 +8,8 @@ const SignUp = () => {
 			<nav className="nav">
 				<img src={logo}></img>
 			</nav>
->>>>>>> nadeen
 		</div>
 	);
 };
 
-<<<<<<< HEAD
-export default OrgSignUp;
-=======
 export default SignUp;
->>>>>>> nadeen
