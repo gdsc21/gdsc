7a2274afd1976382b7b4f20ccf7fb80726a5ac59
Timothee Legros <timothee@ualberta.ca>
2021-03-14 22:05:56 +0100

    webhook test

--- backend/functions/API/gitHub.js
+++ backend/functions/API/gitHub.js
@@ -2,13 +2,28 @@
 
 
 exports.pushes = (request, response) => {
-    console.log(request)
-    return response.status(200).json({body: request.body})
-    // let data
-    // if (typeof request.body != "object")
-    //     data = JSON.parse(request.body)
-    // else data = request.body
+    // return response.status(200).json({body: request.body})
+    let data
+    if (typeof request.body != "object")
+        data = JSON.parse(request.body)
+    else data = request.body
+
+    let repository = data["repository"]
+    let commits = data["commits"]
+
+    // let payload = {}
+    // commits.forEach((commit) => {
     //
+    // })
+
+    let payload
     // console.log(data.payload)
-    // return response.status(200).json({payload: data.payload})
+    const Http = new XMLHttpRequest()
+    Http.onreadystatechange = () => {
+        if (this.readyState === 4 && this.status === 200) {
+            payload = this.response
+        }
+    }
+
+    return response.status(200).json({commits: commits, payload: payload})
 }
