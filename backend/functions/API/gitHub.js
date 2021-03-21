b1f740beab3b14f4ebd8cc84e39a132a89183d49
Timothee Legros <timothee@ualberta.ca>
2021-03-21 08:55:22 +0100

    wh working

--- backend/functions/API/gitHub.js
+++ backend/functions/API/gitHub.js
@@ -22,103 +22,105 @@ exports.push = async (request, response) => {
      * Receives the post request from GitHub push event and saves each commits info in commitArr then saves this list
      * in the request body and passes on to the next function
      */
-    return response.status(200)
-    // let data
-    // if (typeof request.body != "object")
-    //     data = JSON.parse(request.body)
-    // else data = request.body
-    //
-    // const appOctokit = new Octokit({
-    //     userAgent: "Push App V1",
-    //     baseUrl: 'https://api.github.com',
-    //     authStrategy: createAppAuth,
-    //     auth: {
-    //         appId: functions.config().gh.appid,
-    //         privateKey: "-----BEGIN RSA PRIVATE KEY-----\n" +
-    //             "MIIEpAIBAAKCAQEA7YWDF2v8zaJd32CoXV0ZHoFMFYllDxpXNyFlYb3dFN6pdEjD\n" +
-    //             "pChE8N9gV0kvSoLg32IwLGVR6o+6hqapHjEkpd9MnR4DWQv0n8QdhJbocZcYIorU\n" +
-    //             "nKyGXSZOLxHTfxhVuw9BiL+hrFz/ElIxS6d4/4ZnPxhoJkce9T6wsMh5yy3qVb2v\n" +
-    //             "pOsU6oPxpP9TtXQcbCBpLENe826H+WGx9572kNzj2hLB5ghmQLY1xK4CN7YUb+4I\n" +
-    //             "HZT6wzUdHZUXQa+SNpuxza2RAohDk1uS5wIs41a/xvSW9IGbj6GL98pfFEDk+eKV\n" +
-    //             "cbP5pE+1644XnwQPKPn5pyUtFVAoab+j3Nza/QIDAQABAoIBAFbprO9sH2RrjmgH\n" +
-    //             "qXQIdgGYlGupC+a738AXo05huD1DwMQBQD2YUqnFQy6NZBWi0IBNII2OQaxQDZPp\n" +
-    //             "9iZLGzrq+9DeQ6FY45l2nMqAoeu2uykgne36n2wEcUV+A2MVV4GMcpGEdbdpjBh7\n" +
-    //             "JPim/nqaBruqxamECsr4tpTpts9meC8iOxGO48+B7XG92mlzAcdlPfxTQsKA+GLG\n" +
-    //             "yeTbBZ+ep/I15ohdK8/eLeDe+bhb1ZpW61ZONyyVc3XGBZLiWejKYnlk29vKdvwV\n" +
-    //             "KrknyLyxkTQLAc58p5sxQtDTM2q2WKYLGQvCKWYkCvki9pDwVZWmev0kaDnzw47H\n" +
-    //             "uxbkT2ECgYEA/19G7TJs/Aml36WlTiZlgQm/gTsl12f0E9hMecyjqI/0xgYwwMXp\n" +
-    //             "oOX+ivmVPBokN85/rgHpAglli4EneoOL1qNZ1EGOP6dTMsxuEcCsLxNYO5gnbU3k\n" +
-    //             "ryN4LSGOfrW1cZ5OuZBUxWlmA9WmfXZF6hdmLjEJ3CCpQyRyfAyJiqUCgYEA7hsA\n" +
-    //             "Gny4Qw/tGfQ23e33DiMz9a9zN717xJGmb8HM9XqGfIymP3Go7+TwYEfOGFphR6Ac\n" +
-    //             "yW++O0mOJrVA28UGnPqUIANT7A2bPU7MjW7xUMj6NyJMcZiJyu+w7wZV9r7LS226\n" +
-    //             "XPRqAWyMJfvAV59Hw0jlJJ0Aqs3mv6rJYh1Xl3kCgYAHpydM/HHfq7pY1XH6wZPR\n" +
-    //             "JiWjDc64gdkCrzy7ebJ93rKLLKxRWp0BwWK7b3dVccMcGQgigtQkx3tPjvNL7J1I\n" +
-    //             "NWT/w2cr6SvJHe8+gPOoBYBjaM/lqqvrw7haQeMvUOq7GO9rCDRCJkJ0Yva2U9EM\n" +
-    //             "jt71C2ssOZ5Y8MKtjQKiMQKBgQCepvcGrxvH85C0vnjgn3MCxIoWpnVLKsKRU8tm\n" +
-    //             "o+eBmcaKrt0HYSCD2DQiszWsHGy9YP5NaluC/ZvuRs+UoE+rwXt5aT4+B0LtMtgx\n" +
-    //             "VT8N6RxwKDZvaohF5DgszDfzVWX4OID49xK7KCyqEnky6TrT8HpeTw7mwJOEGrRc\n" +
-    //             "39hBYQKBgQDv4Abq+K6NmT6KXU7ImfIEIn/rlXzJiD+rJVTDLBfPgCWBbQ54Tn9C\n" +
-    //             "71peY6U5FbtwaWwR7BPpPUDKPOUE8IzVFg1eZnet4EpHLHEc1eDPbyvbveVsozWH\n" +
-    //             "VFkE5jcwfQJIcqFReq0/22e+zfbXphk4lAXhb0W8+2WDeRPWPFKRWw==\n" +
-    //             "-----END RSA PRIVATE KEY-----",
-    //         installationId: data.installation.id,
-    //         clientId: functions.config().gh.clientid,
-    //         clientSecret: functions.config().gh.clientsecret
-    //     }
-    // })
-    //
-    // try {
-    //     const { token } = await appOctokit.auth({
-    //         type: "installation",
-    //     })
-    // } catch {
-    //     return response.status(404).json({error: "Authentication with GitHub failed"})
-    // }
-    //
-    // let commitArr = []
-    // for (let i=0; i < data.commits.length; i++) {
-    //     let res, commitStats
-    //     let commit = data.commits[i]
-    //     try {
-    //         res = await appOctokit.repos.getCommit({
-    //             owner: data.repository.owner.login,
-    //             repo: data.repository.name,
-    //             ref: commit.id
-    //         })
-    //
-    //         if (res.status != 200) return response.status(res.status).json({error: "Problem requesting commit"})
-    //         commitStats = res.data.stats
-    //
-    //     } catch {
-    //         return response.status(400).json({error: "Commit not found"})
-    //     }
-    //
-    //     if (commitStats === "undefined") return response.status(400).json({error: "No stats"})
-    //     commitArr.push({
-    //         [commit.id]: {
-    //             "repo": {
-    //                 "repoID": data.repository.id,
-    //                 "repoName": data.repository.name
-    //             },
-    //             "authorEmail" : commit.author.email,
-    //             "authorName": commit.author.username,
-    //             "timestamp": commit.timestamp,
-    //             "Message": commit.message,
-    //             "url": commit.url,
-    //             "changes": {
-    //                 "additions": commitStats.additions,
-    //                 "deletions": commitStats.deletions,
-    //                 "total": commitStats.total
-    //             }
-    //         }
-    //     })
-    // }
-    //
-    // try {
-    //     request.body = {commits: commitArr}
-    // } catch {
-    //     return response.status(400).json({error: "Commit save to body error"})
-    // }
+
+    let data
+    if (typeof request.body != "object")
+        data = JSON.parse(request.body)
+    else data = request.body
+
+    const appOctokit = new Octokit({
+        userAgent: "Push App V1",
+        baseUrl: 'https://api.github.com',
+        authStrategy: createAppAuth,
+        auth: {
+            appId: functions.config().gh.appid,
+            privateKey: "-----BEGIN RSA PRIVATE KEY-----\n" +
+                "MIIEpAIBAAKCAQEA7YWDF2v8zaJd32CoXV0ZHoFMFYllDxpXNyFlYb3dFN6pdEjD\n" +
+                "pChE8N9gV0kvSoLg32IwLGVR6o+6hqapHjEkpd9MnR4DWQv0n8QdhJbocZcYIorU\n" +
+                "nKyGXSZOLxHTfxhVuw9BiL+hrFz/ElIxS6d4/4ZnPxhoJkce9T6wsMh5yy3qVb2v\n" +
+                "pOsU6oPxpP9TtXQcbCBpLENe826H+WGx9572kNzj2hLB5ghmQLY1xK4CN7YUb+4I\n" +
+                "HZT6wzUdHZUXQa+SNpuxza2RAohDk1uS5wIs41a/xvSW9IGbj6GL98pfFEDk+eKV\n" +
+                "cbP5pE+1644XnwQPKPn5pyUtFVAoab+j3Nza/QIDAQABAoIBAFbprO9sH2RrjmgH\n" +
+                "qXQIdgGYlGupC+a738AXo05huD1DwMQBQD2YUqnFQy6NZBWi0IBNII2OQaxQDZPp\n" +
+                "9iZLGzrq+9DeQ6FY45l2nMqAoeu2uykgne36n2wEcUV+A2MVV4GMcpGEdbdpjBh7\n" +
+                "JPim/nqaBruqxamECsr4tpTpts9meC8iOxGO48+B7XG92mlzAcdlPfxTQsKA+GLG\n" +
+                "yeTbBZ+ep/I15ohdK8/eLeDe+bhb1ZpW61ZONyyVc3XGBZLiWejKYnlk29vKdvwV\n" +
+                "KrknyLyxkTQLAc58p5sxQtDTM2q2WKYLGQvCKWYkCvki9pDwVZWmev0kaDnzw47H\n" +
+                "uxbkT2ECgYEA/19G7TJs/Aml36WlTiZlgQm/gTsl12f0E9hMecyjqI/0xgYwwMXp\n" +
+                "oOX+ivmVPBokN85/rgHpAglli4EneoOL1qNZ1EGOP6dTMsxuEcCsLxNYO5gnbU3k\n" +
+                "ryN4LSGOfrW1cZ5OuZBUxWlmA9WmfXZF6hdmLjEJ3CCpQyRyfAyJiqUCgYEA7hsA\n" +
+                "Gny4Qw/tGfQ23e33DiMz9a9zN717xJGmb8HM9XqGfIymP3Go7+TwYEfOGFphR6Ac\n" +
+                "yW++O0mOJrVA28UGnPqUIANT7A2bPU7MjW7xUMj6NyJMcZiJyu+w7wZV9r7LS226\n" +
+                "XPRqAWyMJfvAV59Hw0jlJJ0Aqs3mv6rJYh1Xl3kCgYAHpydM/HHfq7pY1XH6wZPR\n" +
+                "JiWjDc64gdkCrzy7ebJ93rKLLKxRWp0BwWK7b3dVccMcGQgigtQkx3tPjvNL7J1I\n" +
+                "NWT/w2cr6SvJHe8+gPOoBYBjaM/lqqvrw7haQeMvUOq7GO9rCDRCJkJ0Yva2U9EM\n" +
+                "jt71C2ssOZ5Y8MKtjQKiMQKBgQCepvcGrxvH85C0vnjgn3MCxIoWpnVLKsKRU8tm\n" +
+                "o+eBmcaKrt0HYSCD2DQiszWsHGy9YP5NaluC/ZvuRs+UoE+rwXt5aT4+B0LtMtgx\n" +
+                "VT8N6RxwKDZvaohF5DgszDfzVWX4OID49xK7KCyqEnky6TrT8HpeTw7mwJOEGrRc\n" +
+                "39hBYQKBgQDv4Abq+K6NmT6KXU7ImfIEIn/rlXzJiD+rJVTDLBfPgCWBbQ54Tn9C\n" +
+                "71peY6U5FbtwaWwR7BPpPUDKPOUE8IzVFg1eZnet4EpHLHEc1eDPbyvbveVsozWH\n" +
+                "VFkE5jcwfQJIcqFReq0/22e+zfbXphk4lAXhb0W8+2WDeRPWPFKRWw==\n" +
+                "-----END RSA PRIVATE KEY-----",
+            installationId: data.installation.id,
+            clientId: functions.config().gh.clientid,
+            clientSecret: functions.config().gh.clientsecret
+        }
+    })
+
+    try {
+        const { token } = await appOctokit.auth({
+            type: "installation",
+        })
+    } catch {
+        return response.status(404).json({error: "Authentication with GitHub failed"})
+    }
+
+    let commitArr = []
+    for (let i=0; i < data.commits.length; i++) {
+        let res, commitStats
+        let commit = data.commits[i]
+        try {
+            res = await appOctokit.repos.getCommit({
+                owner: data.repository.owner.login,
+                repo: data.repository.name,
+                ref: commit.id
+            })
+
+            if (res.status != 200) return response.status(res.status).json({error: "Problem requesting commit"})
+            commitStats = res.data.stats
+
+        } catch {
+            return response.status(400).json({error: "Commit not found"})
+        }
+
+        if (commitStats === "undefined") return response.status(400).json({error: "No stats"})
+        commitArr.push({
+            [commit.id]: {
+                "repo": {
+                    "repoID": data.repository.id,
+                    "repoName": data.repository.name
+                },
+                "authorEmail" : commit.author.email,
+                "authorName": commit.author.username,
+                "timestamp": commit.timestamp,
+                "Message": commit.message,
+                "url": commit.url,
+                "changes": {
+                    "additions": commitStats.additions,
+                    "deletions": commitStats.deletions,
+                    "total": commitStats.total
+                }
+            }
+        })
+    }
+
+    try {
+        request.body = {commits: commitArr}
+    } catch {
+        return response.status(400).json({error: "Commit save to body error"})
+    }
+
+    return response.status(200).json({error: "immediate return"})
     // return next()
 }
 
