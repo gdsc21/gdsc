18a9fbf59dbe73d56873fba3793372f33dc76bb8
Gokul Sivadasan <gokulsivadasan@hotmail.com>
2021-03-27 16:27:16 -0700

    non profit styling (except carousel)

---
+++ vol_dev/src/pages/nonprofit/Components/devnav.js
@@ -0,0 +1,13 @@
+import {Link} from 'react-router-dom'
+const DevNav = () => {
+    return ( 
+        <nav className="devNav">
+            <div><Link to="/dashboard"><span>Dashboard</span></Link></div>
+            <div><Link to="/"><span>Notifications</span></Link></div>
+            <div><Link to="/"><span>Messages</span></Link></div>
+            <div><Link to="/explore"><span>Explore</span></Link></div>    
+        </nav>
+     );
+}
+ 
+export default DevNav;
